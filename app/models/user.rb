d64177ee5fb9259b3f87ef0b03935d8c0d1a2080
aoba <ipbdipbd00@gmail.com>
2019-03-17 15:48:37 +0900

    Fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@
 # Table name: users
 #
 #  id              :integer          not null, primary key
+#  birthday        :date
 #  name            :string(255)      not null
 #  password_digest :string(255)      not null
 #  created_at      :datetime         not null
@@ -27,4 +28,9 @@ class User < ApplicationRecord
   # 最低文字数を設定すると、空の時もエラーが返されるため、presenceを設定する必要がない
   validates :password,
     length: { minimum: 8 }
+
+  def age
+    now = Time.zone.now
+    (now.strftime('%Y%m%d').to_i - birthday.strftime('%Y%m%d').to_i) / 10000
+  end
 end
